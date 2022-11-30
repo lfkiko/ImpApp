a8235ec35e5830ecc863cb122051ca5caea435c8
lior_feigenbaum <lior.feigenbaum@personetics.com>
2022-11-30 13:30:18 +0200

    S-00036 - __pycache__ cleanUP.py howToExcute.txt main.kv main.py replayScript.json Scripts venv Removed build and dist directories. __pycache__ cleanUP.py howToExcute.txt main.kv main.py replayScript.json Scripts venv Removed main.spec

---
+++ Scripts/appleTwoApple/apple2Apple.py
@@ -0,0 +1,20 @@
+import sys
+from logging import info
+
+
+def main(argv):
+    info("Starting apple two apple")
+    differnce = {}
+    oldApple = {}
+    newApple = {}
+    for user in oldApple:
+        pass
+    info("Apple two apple is finished")
+    pass
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
+#   0 - old versioins file
+#   1 - new versioins file
