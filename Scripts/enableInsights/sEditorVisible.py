c248103d1c9bed26e0840bdb972873bf1dcb23e9
lior_feigenbaum <lior.feigenbaum@personetics.com>
2022-12-12 14:31:16 +0200

    S-00045 - __pycache__ cleanUP.py howToExcute.txt main.kv main.py replayScript.json requirements.txt Scripts venv remove print from sEditorVisible.py

--- Scripts/enableInsights/sEditorVisible.py
+++ Scripts/enableInsights/sEditorVisible.py
@@ -13,10 +13,7 @@ def modifyJson(fileName, jsonData):
     data = readCsv(fileName)
     insightsList = getCol(fileName, 'insight')
     remove = []
-    # print(data)
-    # print(data['insight'])
-    # print(data['UC'])
-    # print(insightsList)
+    
     for insight in jsonData['visible'].keys():
         if insight not in insightsList:
             remove.append(insight)
@@ -33,18 +30,6 @@ def modifyJson(fileName, jsonData):
         for i in uc:
             new = insight + "_UC" + i
             jsonData["visible"][insight].append(new)
-    
-    # for insight in
-    prettyPrintJson(jsonData)
-    # for i in data.index:
-    #     json_data["visible"][data['insight'][i]] = []
-    #     uc = data['UC'][i].split(",")
-    #     for j in uc:
-    #         if len(uc) == 1:
-    #             new = data['insight'][i] + "_UC1"
-    #         else:
-    #             new = data['insight'][i] + "_UC" + j
-    #         json_data["visible"][data['insight'][i]].append(new)
     return jsonData
 
 
@@ -56,8 +41,6 @@ def main(argv):
         error('Path Error:' + e.__str__()[e.__str__().index(']') + 1:])
         return
     jsonData = json.loads(readJsonZip(getPath('DataLoad'), 'product-editor-engage-biz-unit.zip', 'SEditorVisible.json'))
-    prettyPrintJson(jsonData)
-    # print(type(jsonData))
     transfer.main([argv[0], 'visible'])
     fileName = getFile(argv[0])
     jsonData = modifyJson(fileName, jsonData)
