df0271cb3c14d4019384800d274250f9b281ad00
lior_feigenbaum <lior.feigenbaum@personetics.com>
2023-06-04 16:19:10 +0300

    S-00056 - __pycache__ cleanUP.py howToExcute.txt Logo.png main.kv main.py persoLogo.png replayScript.json requirements.txt Scripts venv don't add attribute if exists in fact

--- Scripts/newFactField/newFactField.py
+++ Scripts/newFactField/newFactField.py
@@ -8,20 +8,23 @@ def updateFacts(insightPath, insightUc, fact, fieldName, fieldType, fieldVal):
     insightFactsPath = os.path.join(insightPath, insightUc, "JInsightFacts.json")
     if os.path.exists(insightPath):
         try:
-            insightFacts = readJson(insightFactsPath)
-        except:
             insightFacts = readJsonUtf8Sig(insightFactsPath)
+        except Exception as e:
+            error(e.__str__())
         if fact in insightFacts.keys():
-            insightFacts[fact]['cols'].append(fieldName)
-            for row in insightFacts[fact]['rows']:
-                row.append(fieldVal)
-            insightFacts[fact]['attributesTypes'].append(fieldType)
-            try:
-                updateJsonMultiLang(insightFactsPath, insightFacts)
-            except:
-                updateJsonMultiLangUtf8Sig(insightFactsPath, insightFacts)
+            if fieldName not in insightFacts[fact]['cols']:
+                insightFacts[fact]['cols'].append(fieldName)
+                for row in insightFacts[fact]['rows']:
+                    row.append(fieldVal)
+                insightFacts[fact]['attributesTypes'].append(fieldType)
+                try:
+                    updateJsonMultiLangUtf8Sig(insightFactsPath, insightFacts)
+                except Exception as e:
+                    error(e.__str__())
+            else:
+                warning(fieldName + ' is already part of ' + fact)
         else:
-            warning(fact + 'is not part of ' + insightPath)
+            warning(fact + ' is not part of ' + insightPath)
     else:
         warning('There is no such file = ' + insightPath)
 
@@ -51,6 +54,7 @@ def main(argv):
     channels = getChannels(solution)
     if len(channels) > 3:
         theChannel = chooseChanel(channels)
+        print(theChannel)
         try:
             solution = os.path.join(solution + theChannel, 'Insights')
         except Exception as e:
@@ -63,6 +67,28 @@ def main(argv):
     insights = os.listdir(solution)
     insights.remove('SEntities')
     checkFacts(solution, insights, fact, fieldName, fieldType, fieldVal)
+    staticFacts = os.path.join(getSolution(getPath('solution')), "SEditorDefinition", "SStaticInsightFacts.json")
+    if os.path.exists(staticFacts):
+        try:
+            insightFacts = readJsonUtf8Sig(staticFacts)
+        except Exception as e:
+            error(e.__str__())
+        if fact in insightFacts['factsData']['dataModels'].keys():
+            if fieldName not in insightFacts['factsData']['dataModels'][fact]['cols']:
+                insightFacts['factsData']['dataModels'][fact]['cols'].append(fieldName)
+                for row in insightFacts['factsData']['dataModels'][fact]['rows']:
+                    row.append(fieldVal)
+                insightFacts['factsData']['dataModels'][fact]['attributesTypes'].append(fieldType)
+                try:
+                    updateJsonMultiLangUtf8Sig(staticFacts, insightFacts)
+                except Exception as e:
+                    error(e.__str__())
+            else:
+                warning(fieldName + ' is already part of staticFacts')
+        else:
+            warning(fact + ' is not part of ' + staticFacts)
+    else:
+        warning("SStaticInsightFacts.json is not overridden")
     endLog()
 
 
